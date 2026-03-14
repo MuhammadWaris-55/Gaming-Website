1f532fa56973f2f6d03003b5a36a8ecff3197a69
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-03-14 23:21:33 +0500

    Fix Features Section

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -42,7 +42,7 @@ const BentoCard = ({ src, title, description }) => {
                 src={src}
                 loop
                 muted
-                autoPlay
+                autoPlay 
                 className='absolute left-0 top-0 size-full object-cover object-center'
             />
             <div className='relative z-10 flex size-full flex-col justify-between p-5 text-blue-50'>
